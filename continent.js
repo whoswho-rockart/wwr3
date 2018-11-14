a51b2abbe37b035993574d9ae01d1c98656cc92b
WhosWho-RockArt <whoswho.rockart@gmail.com>
2018-11-14 15:27:14 +0000

    wwr3 update

---
+++ continent.js
@@ -0,0 +1,11 @@
+var continent = {
+// planetarynames.wr.usgs.gov/Abbreviations
+    "": "",
+    "AF": "Africa",
+    "AN": "Antarctica",
+    "AS": "Asia",
+    "EU": "Europe",
+    "NA": "North America",
+    "SA": "South America",
+    "OC": "Oceania"
+};
